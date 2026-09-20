fcbb972e039fb8e46c941733e9eced6ed261fec3
Qi Gao <qi.gao2012@gmail.com>
2026-09-20 20:56:52 +0800

    refactor(charts): use direct Salts JSON parser API

--- charts/mermaid/xychart/src/xychart_parser_wrapper.c
+++ charts/mermaid/xychart/src/xychart_parser_wrapper.c
@@ -59,42 +59,42 @@ XYDiagram* xychart_parse(const char* input) {
 }
 
 // --- JSON Serialization ---
-#include "turbo_parser.h"
+#include "json_parser.h"
 
 static json_value_t* serialize_axis(XYAxis* axis) {
-    json_value_t* obj = turbo_json_create_object();
+    json_value_t* obj = json_create_object();
     if (axis->title) {
-        turbo_json_object_set_string(obj, "title", axis->title);
+        json_object_set_string(obj, "title", axis->title);
     }
-    turbo_json_object_set_bool(obj, "hasRange", axis->has_range);
+    json_object_set_bool(obj, "hasRange", axis->has_range);
     if (axis->has_range) {
-        turbo_json_object_set_number(obj, "rangeMin", axis->range_min);
-        turbo_json_object_set_number(obj, "rangeMax", axis->range_max);
+        json_object_set_number(obj, "rangeMin", axis->range_min);
+        json_object_set_number(obj, "rangeMax", axis->range_max);
     }
     if (axis->categories.count > 0) {
-        json_value_t* arr = turbo_json_create_array();
+        json_value_t* arr = json_create_array();
         for (int i = 0; i < axis->categories.count; i++) {
-            turbo_json_array_add(arr, turbo_json_create_string(axis->categories.items[i]));
+            json_array_add(arr, json_create_string(axis->categories.items[i]));
         }
-        turbo_json_object_add(obj, "categories", arr);
+        json_object_add(obj, "categories", arr);
     }
     return obj;
 }
 
 static json_value_t* serialize_series(XYSeries* series) {
-    json_value_t* arr = turbo_json_create_array();
+    json_value_t* arr = json_create_array();
     while (series) {
-        json_value_t* obj = turbo_json_create_object();
-        turbo_json_object_set_string(obj, "type", series->type == XY_SERIES_LINE ? "line" : "bar");
+        json_value_t* obj = json_create_object();
+        json_object_set_string(obj, "type", series->type == XY_SERIES_LINE ? "line" : "bar");
         if (series->name) {
-            turbo_json_object_set_string(obj, "name", series->name);
+            json_object_set_string(obj, "name", series->name);
         }
-        json_value_t* data_arr = turbo_json_create_array();
+        json_value_t* data_arr = json_create_array();
         for (int i = 0; i < series->data.count; i++) {
-            turbo_json_array_add(data_arr, turbo_json_create_number(series->data.items[i]));
+            json_array_add(data_arr, json_create_number(series->data.items[i]));
         }
-        turbo_json_object_add(obj, "data", data_arr);
-        turbo_json_array_add(arr, obj);
+        json_object_add(obj, "data", data_arr);
+        json_array_add(arr, obj);
         series = series->next;
     }
     return arr;
@@ -103,23 +103,23 @@ static json_value_t* serialize_series(XYSeries* series) {
 char* xychart_to_json(XYDiagram* diagram) {
     if (!diagram) return NULL;
 
-    json_value_t* root = turbo_json_create_object();
-    turbo_json_object_set_string(root, "type", "xychart");
+    json_value_t* root = json_create_object();
+    json_object_set_string(root, "type", "xychart");
 
-    if (diagram->title) turbo_json_object_set_string(root, "title", diagram->title);
-    if (diagram->accTitle) turbo_json_object_set_string(root, "accTitle", diagram->accTitle);
-    if (diagram->accDescr) turbo_json_object_set_string(root, "accDescr", diagram->accDescr);
+    if (diagram->title) json_object_set_string(root, "title", diagram->title);
+    if (diagram->accTitle) json_object_set_string(root, "accTitle", diagram->accTitle);
+    if (diagram->accDescr) json_object_set_string(root, "accDescr", diagram->accDescr);
 
-    turbo_json_object_set_string(root, "orientation", 
+    json_object_set_string(root, "orientation", 
         diagram->orientation == XY_ORIENTATION_VERTICAL ? "vertical" : "horizontal");
 
-    turbo_json_object_add(root, "xAxis", serialize_axis(&diagram->xAxis));
-    turbo_json_object_add(root, "yAxis", serialize_axis(&diagram->yAxis));
+    json_object_add(root, "xAxis", serialize_axis(&diagram->xAxis));
+    json_object_add(root, "yAxis", serialize_axis(&diagram->yAxis));
 
     if (diagram->series) {
-        turbo_json_object_add(root, "series", serialize_series(diagram->series));
+        json_object_add(root, "series", serialize_series(diagram->series));
     }
 
     size_t len;
-    return turbo_json_serialize_pretty_crlf(root, &len);
+    return json_serialize_pretty_crlf(root, &len);
 }
