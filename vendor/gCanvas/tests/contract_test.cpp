f96bdd7faecd5a9e5f815400aa819562fa548d7c
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 00:27:44 +0800

    test(gcanvas): expose UTF-8 measurement contract

--- vendor/gCanvas/tests/contract_test.cpp
+++ vendor/gCanvas/tests/contract_test.cpp
@@ -43,6 +43,10 @@ static_assert(
     std::is_same_v<decltype(std::declval<const gcanvas::Window&>().has_pointer_capture()), bool>);
 static_assert(std::is_same_v<decltype(std::declval<gcanvas::Window&>().get_content_scale()),
                              gcanvas::vec2>);
+static_assert(std::is_same_v<
+              decltype(std::declval<gcanvas::Context&>().measure_text(
+                  std::declval<const std::string&>())),
+              gcanvas::vec2>);
 
 int main()
 {
