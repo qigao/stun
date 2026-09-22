1b791729420ab2487abcb23aa2786b501c7bdbbd
Qi Gao <qi.gao2012@gmail.com>
2026-09-22 11:40:32 +0800

    refactor(gcanvas): give GL profiles unique private namespaces

--- vendor/gCanvas/src/backends/gl/gl_api.hpp
+++ vendor/gCanvas/src/backends/gl/gl_api.hpp
@@ -4,8 +4,10 @@
 #if defined(GCANVAS_GL_DESKTOP) && defined(GCANVAS_GL_GLES)
 #error "Exactly one gCanvas GL profile must be selected"
 #elif defined(GCANVAS_GL_DESKTOP)
+#define GCANVAS_GL_PROFILE_NAMESPACE desktop_gl
 #include <glad/glad.h>
 #elif defined(GCANVAS_GL_GLES)
+#define GCANVAS_GL_PROFILE_NAMESPACE opengles_gl
 #include <GLES3/gl3.h>
 #include <GLES3/gl3ext.h>
 #else
